ee4e26e7b1d4d9c0a08bf500909e6d4653e6b8f2
mattbues <61145020+mattbues@users.noreply.github.com>
2020-03-26 15:56:40 +0100

    Add files via upload

--- webapp/treatment/database_connection.php
+++ webapp/treatment/database_connection.php
@@ -1,1 +1,15 @@
-<?php ?>
+<?php 
+
+
+function connectToDb() {
+	try {
+    	$db = new PDO('mysql:host=localhost; dbname=e-mergency', 'root', '');
+  	}
+  	catch(exception $e) {
+    	die('Error '.$e->getMessage());
+  	}
+
+  	return db;
+}
+ 
+?>
