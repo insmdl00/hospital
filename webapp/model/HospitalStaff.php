53c8502ddb7ee6e75bd8ec1f581321d28b6f5790
mattbues <61145020+mattbues@users.noreply.github.com>
2020-03-27 15:04:22 +0100

    Update HospitalStaff.php

--- webapp/model/HospitalStaff.php
+++ webapp/model/HospitalStaff.php
@@ -7,6 +7,15 @@ class HospitalStaff : Person {
 	private $_sector;
 
 	public function __construct($id, $email, $password, $lastName, $firstName, $birthdate, $city, $cityCode, $phone, $job, $sector) {
+		$this->$_id = $id;
+		$this->$_email = $email;
+		$this->$_password = $password;
+		$this->$_lastName = $lastName;
+		$this->$_firstName = $firstName;
+		$this->$_birthdate = $birthdate;
+		$this->$_city = $city;
+		$this->$_cityCode = $cityCode;
+		$this->$_phone = $phone;
 		$this->$_job = $job;
 		$this->$_sector = $sector;
 	}
