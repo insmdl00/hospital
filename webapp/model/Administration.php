05b60e4ad2df721c78911ea4fb436e1bfd50fb78
mattbues <61145020+mattbues@users.noreply.github.com>
2020-03-27 15:03:58 +0100

    Update Administration.php

--- webapp/model/Administration.php
+++ webapp/model/Administration.php
@@ -5,7 +5,15 @@ require_once('Person.php');
 class Administration : Person {
 
 	public function __construct($id, $email, $password, $lastName, $firstName, $birthdate, $city, $cityCode, $phone) {
-
+		$this->$_id = $id;
+		$this->$_email = $email;
+		$this->$_password = $password;
+		$this->$_lastName = $lastName;
+		$this->$_firstName = $firstName;
+		$this->$_birthdate = $birthdate;
+		$this->$_city = $city;
+		$this->$_cityCode = $cityCode;
+		$this->$_phone = $phone;
 	}
 
 
